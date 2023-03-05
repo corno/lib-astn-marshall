557d72befdb78986fe54762c88fb54746aea0cbb
corno <corno.astn.a@schraverus.com>
2023-03-05 13:16:41 +0100

    .

--- pareto/src/data/glossary.data.ts
+++ pareto/src/data/glossary.data.ts
@@ -7,7 +7,7 @@ import {
     reference,
     boolean,
     typeReference,
-    dictionary, group, member, taggedUnion, types, func, data, interfaceReference, inf, method, computed, type
+    dictionary, group, member, taggedUnion, types, func, data, interfaceReference, inf, computed, type
 } from "lib-pareto-typescript-project/dist/submodules/glossary/shorthands"
 
 import * as mglossary from "lib-pareto-typescript-project/dist/submodules/glossary"
@@ -21,17 +21,18 @@ export const $: mglossary.T.Glossary<string> = {
             "dictionary": member(computed(dictionary(reference("MarshallableValue")))),
             "group": member(computed(array(group({
                 "key": member(string()),
-                "value": member(reference("MarshallableValue"))
+                "value": member(reference("MarshallableValue")),
             })))),
             "list": member(computed(array(reference("MarshallableValue")))),
             "taggedUnion": member(computed(array(group({
                 "option": member(string()),
-                "value": member(reference("MarshallableValue"))
+                "value": member(reference("MarshallableValue")),
             })))),
             "simpleString": member(computed(string())),
             "multilineString": member(computed(array(string()))),
         })),
     }),
+    'builders': d({}),
     'interfaces': d({
     }),
     'functions': d({
